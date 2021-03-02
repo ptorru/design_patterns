558dbfe7a1272adc33aa3ac984cc8d4585244943
Pedro Torruella <ptorru14@gmail.com>
2021-03-02 22:48:38 +0000

    Added correct file.

---
+++ mer_fsm/mer_fsm.c
@@ -0,0 +1,116 @@
+// The following aims to be a very simple implementation of a
+// Multiple Event Receptor State Machine
+//
+// Inspired by Chapter 5.4 from
+// Design Patterns for Embedded Systems in C
+// by Bruce Powell Douglass
+
+
+#include <stdio.h>
+#include <assert.h>
+#include <ctype.h>
+
+#define MAXCHAR (7)
+
+
+typedef enum state_s
+{
+    IDLE,
+    RECEIVING,
+    PRINTING
+} state_t;
+
+
+typedef struct fsm_s
+{
+    state_t state;
+    char buffer[MAXCHAR];
+    int count;
+} fsm_t;
+
+
+// The following functions implement the transtions into the
+// states
+void into_idle(fsm_t *self)
+{
+    self->state = IDLE;
+    self->count = 0;
+}
+
+
+void into_receiving(fsm_t *self, char c)
+{
+    assert( self->count < MAXCHAR);
+    self->state = RECEIVING;
+    self->buffer[self->count] = c;
+    self->count++;
+}
+
+
+int into_printing(fsm_t *self)
+{
+    assert(self->count > 0);
+    self->state = PRINTING;
+    printf("%c", self->buffer[MAXCHAR-self->count]);
+    self->count--;
+    if(self->count != 0) return 0;
+    printf("\n");
+    return 1;
+}
+
+
+// The following functions are called depending on each event
+void ev_number(fsm_t *self, char c)
+{
+    switch(self->state)
+    {
+    case IDLE:
+	into_receiving(self,c);
+	return;
+    case RECEIVING:
+	into_receiving(self,c);
+	if(self->count == MAXCHAR) into_printing(self);
+	return;
+    }
+}
+
+
+int ev_other(fsm_t *self, char c_in)
+{
+    into_idle(self);
+    if(c_in == 'q') return 1;
+    return 0;
+}
+
+
+int main()
+{
+    char c_in;
+    fsm_t fsm;
+
+    into_idle(&fsm);
+    
+    printf("Insert some numbers and we will encode them for you...\n");
+    while(1)
+    {
+	if(fsm.state != PRINTING)
+	{
+	    printf("$: ");
+	    scanf(" %c", &c_in);
+
+	    if( isdigit(c_in))
+	    {
+	        ev_number(&fsm,c_in);
+	    }
+	    else
+	    {
+	        // Exit if ev_other returns 1
+	        if(ev_other(&fsm, c_in)) return 0;
+	    }
+	}
+	else
+	{
+	    if(into_printing(&fsm)) return 0;
+	}
+    }
+}
